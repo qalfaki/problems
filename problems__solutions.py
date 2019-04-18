67316a25d5c43c7de745bd68f4182178ef3f5842
512B <37502411+512B@users.noreply.github.com>
2019-04-18 09:40:20 +0300

    remove unused variable

--- problems__solutions.py
+++ problems__solutions.py
@@ -1,9 +1,7 @@
 #!usr/bin/python
 
-y = int(input('Enter number '))
-
-
 memo = {}
+
 
 def memoize(f):
     def wrapper(x):
@@ -25,23 +23,29 @@ def prime(x):
         return False
 
 
-
 def b_search(alist, item):
     if len(alist) == 0:
-       return False
+        return False
     else:
-        midpoint = int(len(alist)/2)
+        midpoint = int(len(alist) / 2)
     if alist[midpoint] == item:
         return test_list.index(item)
     else:
-        if item<alist[midpoint]:
-            return b_search(alist[:midpoint],item)
+        if item < alist[midpoint]:
+            return b_search(alist[:midpoint], item)
         else:
-            return b_search(alist[midpoint+1:],item)
+            return b_search(alist[midpoint + 1:], item)
 
-test_list = [0,1,2,3]
 
-print('binary search args; {}, {}. found item at index {}'.format(test_list, 2 , b_search(test_list, 2)))
+test_list = [0, 1, 2, 3]
 
-for i in [1,2,2,2,2,3,4,4,4,4,4,5]:
+print(
+    'binary search args; {}, {}. found item at index {}'.format(
+        test_list,
+        2,
+        b_search(
+            test_list,
+            2)))
+
+for i in [1, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5]:
     print('is {} prime: {}, memorized: {}'.format(i, prime(i), memo))
